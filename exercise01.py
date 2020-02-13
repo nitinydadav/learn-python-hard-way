28abb6c77f9687a47cfaab1dea0133f0b39694cd
nitinydadav <nitin.yad005@gmail.com>
2020-02-13 20:39:02 +0530

    pythin exercise

---
+++ exercise01.py
@@ -0,0 +1,27 @@
+# learn-python-hard-way
+
+learning python.
+
+string_1 = "learn"
+string_2 = "python"
+string_3 = "for"
+string_4 = "fun and profit"
+
+output = string_1 + string_2 + string_3 + string_4
+print(output)
+
+# calulate the length of string_5 
+  string_5 "learn python for fun and profit"
+
+
+  str = "learn python for fun and profit"
+  output(len(str))
+  print(output)
+
+#  reverse string  
+                 string_1
+
+str= "learn"
+output str[-1] + str[-2] + str[-3] + str[-4] + str[-5]
+print output
+
