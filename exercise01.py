b5b627eb278fa881ae61c904d452007a4d6f2ac5
nitinydadav <nitin.yad005@gmail.com>
2020-02-16 14:23:55 +0530

    remove code in README.md file and execise01.py updated

--- exercise01.py
+++ exercise01.py
@@ -1,27 +1,17 @@
 # learn-python-hard-way
 
-learning python.
-
 string_1 = "learn"
 string_2 = "python"
 string_3 = "for"
 string_4 = "fun and profit"
 
-output = string_1 + string_2 + string_3 + string_4
-print(output)
+string_5 = string_1 + string_2 + string_3 + string_4
+print(string_5)
 
 # calulate the length of string_5 
-  string_5 "learn python for fun and profit"
-
-
-  str = "learn python for fun and profit"
-  output(len(str))
-  print(output)
+print(len(string_5))
 
 #  reverse string  
-                 string_1
-
-str= "learn"
-output str[-1] + str[-2] + str[-3] + str[-4] + str[-5]
-print output
+#output = str[-1] + str[-2] + str[-3] + str[-4] + str[-5]
+#print(output)
 
