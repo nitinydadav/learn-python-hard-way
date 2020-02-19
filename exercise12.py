6f1748b8ca0d679c8ce50c12cf97a2c2bc21d6a9
nitinydadav <nitin.yad005@gmail.com>
2020-02-19 20:45:55 +0530

    use raw string

---
+++ exercise12.py
@@ -0,0 +1,18 @@
+# Asking Questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+print "So, you're %r old, %r tall and %r heavy." % (
+age, height, weight)
+
+# output
+"""
+How old are you? 30
+How tall are you? 5.5
+How much do you weigh? 55
+So, you're '30' old, '5.5' tall and '55' heavy.
+"""
