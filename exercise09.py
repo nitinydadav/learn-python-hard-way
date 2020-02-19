d86622f3403747183fc2c5c03cd6c92ca03c516e
nitinydadav <nitin.yad005@gmail.com>
2020-02-19 11:44:46 +0530

    printing

---
+++ exercise09.py
@@ -0,0 +1,23 @@
+# printing printing 
+
+formatter = "%r %r %r %r"
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+"I had this thing.",
+"That you could type up right.",
+"But it didn't sing.",
+"So I said goodnight."
+)
+
+# output
+
+"""
+1 2 3 4
+'one' 'two' 'three' 'four'
+True False False True
+'%r %r %r %r' '%r %r %r %r' '%r %r %r %r' '%r %r %r %r'
+'I had this thing.' 'That you could type up right.' "But it didn't sing." 'So I said goodnight.'
+"""
