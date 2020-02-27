b1024884ed99bd4fe9968b4f4deaa1e435a1dbf3
nitinydadav <nitin.yad005@gmail.com>
2020-02-27 12:16:19 +0530

    Uses argument

--- exercise13.py
+++ exercise13.py
@@ -2,6 +2,12 @@
 
 from sys import argv
 script, first, second, third = argv
+print "argv: ", 
+print "argv: type", type(argv)
+print "argv:0 ", argv[0]
+print "argv:1 ", argv[1]
+print "argv:2 ", argv[2]
+print "argv:3 ", argv[3]
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
