4cbbadd83564a14e8f120132cd3e4c41c247aa3b
nitinydadav <nitin.yad005@gmail.com>
2020-03-02 15:58:13 +0530

    tuple use with function count nd index

--- tuplemethods.py
+++ tuplemethods.py
@@ -8,3 +8,29 @@ print n.count(321)
 print "\n"
 
 print n.count(333)
+
+
+
+# uses of index 
+
+y = ('boski','nav', 'yad', 'boski')
+print "\n"
+
+print y
+
+print "\n"
+
+print y.index("boski")
+
+print "\n"
+
+print y.index('yad')
+print y.index("abc")
+
+"""
+ValueError                                Traceback (most recent call last)
+<ipython-input-83-6eedd2b68194> in <module>()
+----> 1 n.index(666)
+
+ValueError: tuple.index(x): x not in tuple
+"""
