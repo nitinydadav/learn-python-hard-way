6d416db65f7315c7b55bb0346a011baffcff65a5
nitinydadav <nitin.yad005@gmail.com>
2020-03-06 15:58:03 +0530

    Use if and eles

---
+++ exercise30.py
@@ -0,0 +1,29 @@
+people = 30
+cars = 40
+buses = 15
+if cars > people:               # cars is greter then 
+	print "We should take the cars."
+elif cars < people:
+	print "We should not take the cars."
+else:
+	print "We can't decide."
+
+if buses > cars:                 # here is not busses greterthen from cars 
+	print "That's too many buses."
+elif buses < cars:
+	print "Maybe we could take the buses."
+else:
+	print "We still can't decide."
+
+
+if people > buses:
+	print "Alright, let's just take the buses."
+
+else:
+	print "Fine, let's stay home then."
+
+
+# when if is true you can see if stetment 
+
+# if stetment is false then you can see eles stetment 
+
