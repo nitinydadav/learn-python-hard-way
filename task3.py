ebcd19e24bebd2869c09dca84edf82f578de6497
nitinydadav <nitin.yad005@gmail.com>
2020-03-12 12:35:10 +0530

    values and convert list into tuple

---
+++ task3.py
@@ -0,0 +1,28 @@
+items_1 = [{"name": "naveen", "age": 12}, {"name": "nitin", "age": 16}, {"name": "ritu", "age": 23}]
+
+
+for q in items_1:
+	q.values()
+	print q.values()	
+
+print "\n"
+q = []
+for w in items_1:
+     x = w.values()
+     q.extend(x)
+print q
+
+z = []
+for q in items_1:
+	w =  q["age"]
+	z.append(w)
+	text = tuple(z)
+print text
+
+
+x=[]
+for w in items_1:
+	s = w["name"]
+	x.append(s)
+	tuplee= tuple(x)
+print tuplee
