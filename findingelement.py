996dcf8556d9429517753bf9d0917f9241fd86b2
nitinydadav <nitin.yad005@gmail.com>
2020-03-26 20:34:47 +0530

    find element

--- findingelement.py
+++ findingelement.py
@@ -1,20 +1,20 @@
 items_1 = [2,3,4,5,6,7,8,9]
 
-z = raw_input("num: ")
+z = input("num: ")
 
-print z,
+print (z)
 
 q = int(z)
 
 
 if q in items_1: 
-	print ("number is present in given list")
+	print("number is present in given list")
 else:
-	print ("number not  present in given list")
+	print("number not  present in given list")
 
 
 
-print "\n"
+print ("\n")
 
 
 
@@ -22,25 +22,16 @@ items = ('boski', 'nav', 'nit', 'jak')
 
 type(items)
 
-print items 	
+print(items)
 
-x = raw_input("enter name: ")
+x = input("enter name: ")
 
-print x
+print(x)
 
 if x in items:
 	print ("your name is true")
 else:
 	print ("your name is false")
-
-items_1 = [{"name": "naveen", "age": 12}, {"name": "nitin", "age": 16}, {"name": "ritu", "age": 23}]
-
-
-for q in items_1:
-	q.values()
-	print q.values()
-	
-
 
 
 
