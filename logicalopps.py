50bac7baea85d781049fdc3f1736536fee39d071
nitinydadav <nitin.yad005@gmail.com>
2020-03-26 20:25:32 +0530

    and , or

---
+++ logicalopps.py
@@ -0,0 +1,28 @@
+a = 30
+b = 20
+c = 30
+d = 60
+# ,,,,and ,,,,,,
+# when both are true then you will got true
+if a>b and c<d:
+    print("ooo i got true ")
+else:
+    print("nooo i got false")
+
+print("\n")
+# when 1st is true and 2ed false then you will got false
+if a>b and c>d:
+    print("is this true ")
+else:
+    print("this is false")
+
+
+print("\n")
+# ,,,,,Or ,,,,,,,,,,,,,,
+# in the or when the 1st true and 2ed  false its will comes true .
+if a>b or c<d:
+    print(" this is true ")
+else:
+    print("this is false")
+
+
