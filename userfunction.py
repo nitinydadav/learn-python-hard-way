b9e364c9b9467ce3f703d9eae6d2ba52fb70ca6a
nitinydadav <nitin.yad005@gmail.com>
2020-03-26 20:09:15 +0530

    function

--- userfunction.py
+++ userfunction.py
@@ -11,5 +11,38 @@ def one(items):
 			odd.append(q)
 	return even, odd
 
-print one(p)	 
+print (one(p))
 		
+
+print ("\n")
+# reverse string 
+p = ["fun", "to", "time", "it's", "bosko","hello"]                                  
+
+def file(p):	
+	p.reverse()
+	return p
+print (file(p))
+
+print ("\n")
+
+
+
+#variable length  argument 
+def file(*one):
+	print(one[0])
+	print(one[1])
+	print(one[2])
+	z = one[0] + one[1] + one [2]
+	return ("this is the sum:", (z))
+print(file (2,5,8,))
+
+	
+print("\n")
+
+
+def file1(x, *one):
+	w = x * one[0] * one [1]
+
+	return ("multiply:",w)
+ 
+print(file1(8,2,4))
