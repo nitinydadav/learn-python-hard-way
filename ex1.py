3e11459952fca8653a5ea9ab049e69bbf4fe454b
nitinydadav <nitin.yad005@gmail.com>
2020-04-08 12:46:09 +0530

    input and exception handling

--- ex1.py
+++ ex1.py
@@ -1,6 +1,13 @@
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weigh? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
 
-print age, type(age)
-print age * height * weight  
+print (age, type(age))
+
+try:
+    sum = int(age) + int(height) + int(weight)
+    print(sum)
+
+except ValueError :
+    print("this is very imp line")
+
