47059047889dbacccb9b65e3bed96faf9a5f7d0c
nitinydadav <nitin.yad005@gmail.com>
2020-04-10 20:09:11 +0530

    watch that comma at the end

--- exercise08.py
+++ exercise08.py
@@ -1,9 +1,9 @@
 # more printing 
 
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
-print "And everywhere that Mary went."
-"." * 10 # what'd that do?
+print ("Mary had a little lamb.")
+print ("Its fleece was white as %s." % 'snow')
+print ("And everywhere that Mary went."
+"." * 10 )# what'd that do?
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,6 +17,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
+print (end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12)
 
 
