704bcf5297fd5f3eaed99a6f42cf49131c47b7c5
nitinydadav <nitin.yad005@gmail.com>
2020-05-04 22:43:33 +0530

    inputs

--- exercise12.py
+++ exercise12.py
@@ -1,12 +1,12 @@
 # Asking Questions
 
-x = raw_input("what's your name? ")
-age = raw_input("how old are you? ")
-city = raw_input("where are you living? ")
-stdy = raw_input("Are you graduate? ")
-cont = raw_input("which country you want to go? ")
-vsa = raw_input ("have you apply vsa? ")
-mny = raw_input("how much money do have? ")
+x = input("what's your name? ")
+age = input("how old are you? ")
+city = input("where are you living? ")
+stdy = input("Are you graduate? ")
+cont = input("which country you want to go? ")
+vsa = input ("have you apply vsa? ")
+mny = input("how much money do have? ")
 
-print "Hello, %r , You are %r old, you'r %r location is, you'r %r, your country %r is right, you have vsa %r that's good and %r mony book your ticket your self." % (x,age,city,stdy,cont,vsa,mny,)
+print ("Hello, %r , You are %r old, you'r %r location is, you'r %r, your country %r is right, you have vsa %r that's good and %r mony book your ticket your self." % (x,age,city,stdy,cont,vsa,mny,))
 
