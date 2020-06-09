5ce3d281d6a6fe2409c850814dace14593278bbd
nitinydadav <nitin.yad005@gmail.com>
2020-06-09 15:30:05 +0530

    call function with input

--- exercise13.py
+++ exercise13.py
@@ -1,17 +1,17 @@
 #Parameters, Unpacking, Variables
 
 from sys import argv
-script, first, second, third = argv
-print "argv: ", 
-print "argv: type", type(argv)
-print "argv:0 ", argv[0]
-print "argv:1 ", argv[1]
-print "argv:2 ", argv[2]
-print "argv:3 ", argv[3]
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
+script,first,second,third = argv
+print("argv: ",)
+print("argv: type", type(argv))
+print("argv:0 ", argv[0])
+print("argv:1 ", argv[1])
+print("argv:2 ", argv[2])
+print("argv:3 ", argv[3])
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third )
 
 
 # output 
---
+++ input_with_def_func.py
@@ -0,0 +1,11 @@
+int = input("chose this name>>  name name1 ")
+def name ():
+    return("hello would ")
+def name1():
+    return("this is name1")
+if int == "name" :
+    print(name())
+elif int == "name1":
+    print(name1())
+else:
+    print("this is wrong name try agin ")
