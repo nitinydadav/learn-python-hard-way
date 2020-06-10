ad0d361a86f58f096a021a69baff87823ad08b35
nitinydadav <nitin.yad005@gmail.com>
2020-06-10 13:36:54 +0530

    division

---
+++ practice_exe3.py
@@ -0,0 +1,8 @@
+#Read two integers and print two lines. The first line should contain integer division, a // b. The second line should contain float division, a / b.
+
+a = int(input("enter the some nub "))
+b = int(input("enter the some nub "))
+c = a//b
+d = a/b
+print(c,"\n",d)
+
