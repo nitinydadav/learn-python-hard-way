983af0ec32dd5fc1b4aa7e5e0b479f496ee53c09
nitinydadav <nitin.yad005@gmail.com>
2020-06-10 13:32:30 +0530

    arithmetic operator

---
+++ practice_exe2.py
@@ -0,0 +1,16 @@
+#Read two integers from STDIN and print three lines where:
+
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+
+
+item = int(input(" enter the nub  >"))
+item1 = int(input("enter the secoend nub "))
+
+add = item + item1
+sub = item - item1
+multi = item * item1
+
+print("add",add,"\n","sub",sub,"\n","multi",multi)
