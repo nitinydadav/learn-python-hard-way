6f4a894b8434dcf58f2fcfb5c26a2eee707f2809
nitinydadav <nitin.yad005@gmail.com>
2020-07-02 03:17:40 +0530

    greater than

--- prctfile.py
+++ prctfile.py
@@ -1,6 +1,6 @@
 
-#a = input("Enter first num ")
-#b = input("Enter second num ")
+a = input("Enter first num ")
+b = input("Enter second num ")
 
 try:
     if int(a) > int(b):
@@ -15,43 +15,8 @@ else:
     print("Two numbers compared successfuly ")
 
 
-class mobile:
-    @classmethod
-    def cls(cls):
-        print("vivo pro" )
-
-xx = mobile
-print(xx.cls())
 
 
-class Sum:
-    def Add(cls,r):
-        cls.ram = r
-        print("RRRAM",cls.ram)
-roof = Sum()
-print(roof.Add("20gb"))
 
-
-#class funk:
- #   def add(cat,x,y):
-
-#        cat.ans = x + y
- #       return(cat.ans)
-
-#root = funk()
-#print(root.add(10,20))
-try:
-    n = int(input("enter your lucky num > "))
-    if n % 2  !=  0:
-        print("weird")
-    else:
-        if n >= 2 and n <= 5:
-            print("not weird ")
-        elif n >= 6 and n <20:
-            print("weird")
-        elif n > 20:
-            print("not weird" )
-except:
-    print ("you can not pass the string")
 
 
