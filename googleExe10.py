bc730d3392bd2197c9b89089f67674004484abd4
nitinydadav <nitin.yad005@gmail.com>
2020-07-02 03:58:02 +0530

    chek out

---
+++ googleExe10.py
@@ -0,0 +1,7 @@
+
+name = "wonderful things."
+name1 = "hacker to you "
+
+a = f"A {name1} \n is someone creative who does  {name}"  #1 use f < this is called f srting   create string
+                                                 #2 you want to add the name  add in this where you want {} into the variabe
+print(a)
