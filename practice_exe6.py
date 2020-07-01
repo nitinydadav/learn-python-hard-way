3b53ee5260b51e0b9614e91eeccc8eb4bb165727
nitinydadav <nitin.yad005@gmail.com>
2020-07-02 02:54:03 +0530

    hackerrank

---
+++ practice_exe6.py
@@ -0,0 +1,14 @@
+#Print Function
+
+#Read an integer N.
+
+#Without using any string methods, try to print the following:
+
+N = int(input("enter the num "))
+for i in range(1,N):
+    print(i,end="")
+
+
+
+
+
