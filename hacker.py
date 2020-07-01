b4a8efe94ba588164407b338e05c2c55bdaac9e8
nitinydadav <nitin.yad005@gmail.com>
2020-07-02 02:49:46 +0530

    mix

---
+++ hacker.py
@@ -0,0 +1,33 @@
+
+
+
+
+class cla :
+    def end(self):
+        var = int (input("enter  "))
+        xx = range(500)
+        cc = range (500 , 600)
+        if var in xx :
+            print(" 1 got it ")
+            print (var)
+        elif var  in cc :
+            print ( "2 my number is not in the list  ")
+            print ( var)
+        elif  not var is xx:
+            print(" 3 thik is the out side " )
+            print (var )
+        else :
+            print ("i got nothing ")
+x = cla()
+x.end()
+
+
+
+
+
+
+
+
+
+
+
