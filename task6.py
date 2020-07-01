636f2497b7ce4e5666ea6a6b1f05a7e2f2f9a68f
nitinydadav <nitin.yad005@gmail.com>
2020-07-02 02:29:03 +0530

    input

---
+++ task6.py
@@ -0,0 +1,13 @@
+
+def fun():
+    a = (input("enter first num  "))
+    b = (input ("what you want to add, sub,multi,divid "))
+
+    if a + b :
+        print( a,"your add amount is  " , b , )
+        print(type(a))
+    elif a-b:
+        print ( " this is sub")
+    elif a *b :
+        print ("this is the multi")
+print(fun())
